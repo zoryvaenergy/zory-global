23e96ce201f920fa6a7f94e6ce8c20bf2d2aeed2
zoryvaenergy <energyzoryva@gmail.com>
2026-08-07 21:03:03 +0530

    mobile login test

--- src/services/user/saveUser.js
+++ src/services/user/saveUser.js
@@ -1,6 +1,30 @@
-import { ref, set } from "firebase/database";
+import { ref, set, update } from "firebase/database";
 import { database } from "../../firebase/firebaseConfig";
 
 export async function saveUser(userId, userData) {
-  await set(ref(database, "users/" + userId), userData);
+
+  // Save User
+  await set(
+    ref(database, "users/" + userId),
+    userData
+  );
+
+  // Save Wallet Index
+  const walletAddress = userData.wallet.address;
+
+  if (walletAddress) {
+
+    await update(ref(database), {
+      [`walletIndex/${walletAddress.toLowerCase()}`]: userId,
+    });
+
+    console.log(
+      "Wallet Index Saved :",
+      walletAddress.toLowerCase(),
+      "=>",
+      userId
+    );
+
+  }
+
 }
